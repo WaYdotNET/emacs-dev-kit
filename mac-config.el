11c39b1af41a7605160851ec4583c88eb6cba540
Carlo Bertini <waydotnet@gmail.com>
2011-10-14 10:18:48 +0200

    modify mac-config's key :D

--- mac-config.el
+++ mac-config.el
@@ -3,7 +3,12 @@
 
 ;; Emacs users obviously have little need for Command and Option keys,
 ;; but they do need Meta and Control
+;; (setq mac-command-modifier 'meta)
+;; (setq mac-option-modifier 'control)
+
+(setq mac-option-key-is-meta nil)
+(setq mac-command-key-is-meta t)
 (setq mac-command-modifier 'meta)
-(setq mac-option-modifier 'control)
+(setq mac-option-modifier nil)
 
 (provide 'mac-config)
